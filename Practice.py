413d716a17ece36b0444b63e469da6a4566d8acb
Jiang Qinghua <qinghua.jiang@tieto.com>
2018-01-25 14:46:59 +0800

    去掉注释

--- Practice.py
+++ Practice.py
@@ -1,7 +1,7 @@
 
 # python 练习题1
 # 有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
-'''
+
 sum =0
 for i in range(1,5):
     for j in range(1,5):
@@ -11,10 +11,10 @@ for i in range(1,5):
                 sum=sum+1
                 
 print "一共可以组成",sum,"个数字"
-'''
+
 # python 练习题5
 # 输入三个整数x,y,z，请把这三个数由小到大输出。
-'''
+
 def abc(mylist):
     for i in range(0,len(mylist)):
         for j in range(0,i+1):
@@ -24,14 +24,14 @@ def abc(mylist):
 
 arr =[5,6,9,8,1,2,3,4]
 print abc(arr)
-'''
+
 '''
 l=[]
 for i in range(3):
     x=int(input("integer:\n"))
     l=l.append(x)
     print l.sorted()
-'''
+
 '''
 # python 练习题6
 # 斐波那契数列, 指的是这样一个数列：0、1、1、2、3、5、8、13、21、34、……。
@@ -83,11 +83,11 @@ for i in range(1,10):
         print "%d * %d = %d" % (i, j, i*j),  #逗号表示不换行， 通配符的使用要注意。
 #        print i,"*",j,"==",i*j,
 
-'''
+
 # python 练习题9
 #题目：暂停一秒输出。
 #程序分析：使用 time 模块的 sleep() 函数。  
-'''
+
 import time 
 myDictionary = {"a": 123, "b": 456, "c":789 }
 
@@ -114,13 +114,13 @@ time.sleep(1)
 print "格式化时间为：", time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()) # 	time.strftime(fmt[,tupletime])接收以时间元组，并返回以可读字符串表示的当地时间，格式由fmt决定。
 time.sleep(1)
 print "格式化时间为：", time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()) 
-'''
+
 
 # python 练习题11
 # 题目：古典问题：有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子总数为多少？
 # 程序分析：兔子的规律为数列1,1,2,3,5,8,13,21....
 #方法1
-'''
+
 def rabbit(months):
     a=[1,1]
     for i in range(2,months):
@@ -130,9 +130,9 @@ def rabbit(months):
 b= rabbit(45)
 for j in range(1,(len(b)+1)):
     print "第%d个月兔子总对数为：" % j,b[j-1]
-'''
+
 #方法2： # 这种写法是 菜鸟教程提供的
-'''
+
 def rabbit1(num):
     f1 = 1
     #第一个月为1
@@ -145,18 +145,18 @@ def rabbit1(num):
             f1,f2 = f2,f1+f2
     return f1
 print rabbit1(36)  # 第三十六月兔子数量
-'''
+
 # python 练习题12
 # 题目：判断101-200之间有多少个素数，并输出所有素数。
 # 程序分析：判断素数的方法：用一个数分别去除2到sqrt(这个数)，如果能被整除，则表明此数不是素数，反之是素数。 
-'''
+
 for i in range(101,200):
     for j in range(2,i):
         if (i % j == 0):
             break
     else:  #这条else语句对应的是for，不是if，这个是python特有的语句。即在for 循环中，如果没有从任何一个break中退出，则会执行和for对应的else，只要从break中退出了，则else部分不执行。
         print i
-'''
+
 # Python 练习实例13
 #题目：打印出所有的"水仙花数"，所谓"水仙花数"是指一个三位数，其各位数字立方和等于该数本身。例如：153是一个"水仙花数"，因为153=1的三次方＋5的三次方＋3的三次方。
 
